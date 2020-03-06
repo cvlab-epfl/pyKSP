c70376659019a79a2e4cd904eb45f39db40ededf
Krzysztof Lis <adynathos@gmail.com>
2020-03-06 11:56:00 +0100

    build instructions

--- README.md
+++ README.md
@@ -3,11 +3,31 @@ Wrapper written by Pierre Baqué from the original "Multiple Object Tracker Usin
 
 ### Usage
 If you want to modify the source code or need recompile the wrapper, enter the command:
+```
+cd pyKSP
+python setup.py build_ext --inplace
+```
 
-	cd pyKSP
-	python setup.py build_ext --inplace
+Or install to your site-packages to make the module available everywhere:
+```
+cd pyKSP
+python setup.py install
+```
 
 The python script in "pyKSP-Example.py", provides an example on how to use pyKSP from Python.
+
+### Dependencies
+
+Python libraries:
+```
+pip install cython numpy matplotlib
+```
+
+[Boost Graph Library](https://www.boost.org/doc/libs/1_72_0/libs/graph/doc/index.html) to build the KSP binary:
+```
+# on ubuntu
+sudo apt install libboost-graph-dev
+```
 
 ### References
 For more information about the KSP algorithm, please check the
--- pyKSP/setup.py
+++ pyKSP/setup.py
@@ -1,14 +1,22 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
- 
+from Cython.Build import cythonize
+
+"""
+Build ksp and import from anywhere:
+    cd pyKSP
+	python setup.py build
+    python setup.py install
+
+Build ksp and import from this directory
+    cd pyKSP
+	python setup.py build_ext --inplace
+"""
+
 setup(
-  name = 'Demos',
-  ext_modules=[ 
-    Extension("ksp", 
-              sources=["kgraph.pyx", "ksp_graph.cpp","ksp_computer.cpp"], # Note, you can link against a c++ library instead of including the source
-              language="c++"),
-    ],
-  cmdclass = {'build_ext': build_ext},
- 
+    name = 'pyKSP',
+    ext_modules=cythonize(Extension("ksp", 
+        sources=["kgraph.pyx", "ksp_graph.cpp","ksp_computer.cpp"], # Note, you can link against a c++ library instead of including the source
+        language="c++",
+    )),
 )
